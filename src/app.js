619b3b21d331007fc5a601f68d29486e571fdae7
Shailesh Hatte <shailesh.hatte@avalara.com>
2020-04-07 13:09:53 +0530

    Initial commit to herku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -48,14 +49,22 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    geocode(req.query.address, (error, {
+        latitude,
+        longitude,
+        location
+    } = {}) => {
         if (error) {
-            return res.send({ error })
+            return res.send({
+                error
+            })
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return res.send({ error })
+                return res.send({
+                    error
+                })
             }
 
             res.send({
@@ -96,6 +105,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
 })
